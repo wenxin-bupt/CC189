b0b5e3ee88293d0979d5e6fc7087b49781fc7dfd
Wen Xin <wx@whensean.com>
2017-09-19 23:17:11 +0800

    1_2 reverse char array (add descriptions)

--- 1_2.cpp
+++ 1_2.cpp
@@ -1,3 +1,10 @@
+/*******************************************************************************
+1.2 Check Permutation
+
+Given two strings,write a method to decide if one is a permutation of the
+other.
+*******************************************************************************/
+
 #include <algorithm>    // std::swap
 
 using namespace std;
